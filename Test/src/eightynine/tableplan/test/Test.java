672b38860938b104969b7f3e260bbdb0156434a7
Tharinda Wickramaarachchi <Tharinda@atlassian.com>
2016-12-30 17:00:18 +0530

    Test Commit

---
+++ Test/src/eightynine/tableplan/test/Test.java
@@ -0,0 +1,12 @@
+package eightynine.tableplan.test;
+
+/**
+ * Created by tharinda on 12/30/2016.
+ */
+public class Test
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Table Plan" );
+    }
+}
